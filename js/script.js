a86ae99e940a93e894457686bdeebd5726abad3e
verowangari <verowangari34@gmail.com>
2022-01-15 08:11:35 +0300

    create constructor for contact

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+//   User Interface Logic
+$(document).ready(function () {
+  $("form#new-contact").submit(function (event) {
+    event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    $("ul#contacts").append("<li> <span class='contacts'>" + newContact.firstName + "</span> </li>");
+
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    console.log(Contact)
+  });
+});
